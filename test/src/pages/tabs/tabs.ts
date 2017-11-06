b2bc846a8662deb500f340fd260c6feb38683b36
zjx2029799 <zjx2029799@gmail.com>
2017-11-06 00:47:58 -0500

    add arrow for content in education #2

--- test/src/pages/tabs/tabs.ts
+++ test/src/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { EducationPage } from '../education/education';
 import { HomePage } from '../home/home';
 
 @Component({
@@ -13,7 +13,7 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab3Root = EducationPage;
 
   constructor() {
 
--- test/www/build/main.js
+++ test/www/build/main.js
@@ -41,7 +41,7 @@ webpackEmptyAsyncContext.id = 148;
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TabsPage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__about_about__ = __webpack_require__(193);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__contact_contact__ = __webpack_require__(194);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__education_education__ = __webpack_require__(194);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__home_home__ = __webpack_require__(195);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -61,7 +61,7 @@ var TabsPage = (function () {
         this.color = "sblue";
         this.tab1Root = __WEBPACK_IMPORTED_MODULE_3__home_home__["a" /* HomePage */];
         this.tab2Root = __WEBPACK_IMPORTED_MODULE_1__about_about__["a" /* AboutPage */];
-        this.tab3Root = __WEBPACK_IMPORTED_MODULE_2__contact_contact__["a" /* ContactPage */];
+        this.tab3Root = __WEBPACK_IMPORTED_MODULE_2__education_education__["a" /* EducationPage */];
     }
     TabsPage.prototype.changeColor = function () {
         if (this.color == "sblue") {
@@ -74,7 +74,7 @@ var TabsPage = (function () {
     return TabsPage;
 }());
 TabsPage = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({template:/*ion-inline-start:"/Users/mengyuxin/Desktop/Test/test/src/pages/tabs/tabs.html"*/'<ion-tabs color={{color}}>\n  <ion-tab [root]="tab1Root" tabTitle="Home" tabIcon="home" (ionSelect)="colorMain()"></ion-tab>\n  <ion-tab [root]="tab2Root" tabTitle="Calendar" tabIcon="calendar"></ion-tab>\n  <ion-tab [root]="tab3Root" tabTitle="Education" tabIcon="book" (ionSelect)="changeColor()"></ion-tab>\n  <ion-tab [root]="tab4Root" tabTitle="Message" tabIcon="contact"></ion-tab>\n\n</ion-tabs>\n'/*ion-inline-end:"/Users/mengyuxin/Desktop/Test/test/src/pages/tabs/tabs.html"*/
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({template:/*ion-inline-start:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\tabs\tabs.html"*/'<ion-tabs color={{color}}>\n\n  <ion-tab [root]="tab1Root" tabTitle="Home" tabIcon="home" (ionSelect)="colorMain()"></ion-tab>\n\n  <ion-tab [root]="tab2Root" tabTitle="Calendar" tabIcon="calendar"></ion-tab>\n\n  <ion-tab [root]="tab3Root" tabTitle="Education" tabIcon="book" (ionSelect)="changeColor()"></ion-tab>\n\n  <ion-tab [root]="tab4Root" tabTitle="Message" tabIcon="contact"></ion-tab>\n\n\n\n</ion-tabs>\n\n'/*ion-inline-end:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\tabs\tabs.html"*/
     }),
     __metadata("design:paramtypes", [])
 ], TabsPage);
@@ -109,7 +109,7 @@ var AboutPage = (function () {
 }());
 AboutPage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-about',template:/*ion-inline-start:"/Users/mengyuxin/Desktop/Test/test/src/pages/about/about.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>\n      Calender\n    </ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"/Users/mengyuxin/Desktop/Test/test/src/pages/about/about.html"*/
+        selector: 'page-about',template:/*ion-inline-start:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\about\about.html"*/'<ion-header>\n\n  <ion-navbar>\n\n    <ion-title>\n\n      Calender\n\n    </ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding>\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\about\about.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
 ], AboutPage);
@@ -122,7 +122,7 @@ AboutPage = __decorate([
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ContactPage; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EducationPage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(33);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -136,20 +136,30 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 
 
-var ContactPage = (function () {
-    function ContactPage(navCtrl) {
+var EducationPage = (function () {
+    function EducationPage(navCtrl) {
         this.navCtrl = navCtrl;
     }
-    return ContactPage;
+    EducationPage.prototype.buttonClick1 = function () {
+        this.content1 = "Diabetes help. Free program provides personal support to MESSA members MESSA members and their dependents with dia... Read more... MESSA rolling out ...";
+    };
+    EducationPage.prototype.buttonClick2 = function () {
+        this.content2 = "That program evolved through the years and the Michigan Education Special Services Association (MESSA) was formed in 1960. We are a not-for-profit";
+    };
+    EducationPage.prototype.buttonClick3 = function () {
+        this.content3 = "MESSA provides a number of group insurance options for our members and local bargaining teams. Please find below links to basic information about many o";
+    };
+    return EducationPage;
 }());
-ContactPage = __decorate([
+EducationPage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-contact',template:/*ion-inline-start:"/Users/mengyuxin/Desktop/Test/test/src/pages/contact/contact.html"*/'<ion-header >\n  <ion-navbar color="sblue">\n    <ion-title>\n      Education\n    </ion-title>\n  </ion-navbar>\n  <ion-toolbar color="sblue">\n     <ion-searchbar color="white"></ion-searchbar>\n  </ion-toolbar>\n\n\n\n<ion-toolbar no-border-top color="sblue">\n    <ion-segment [(ngModel)]="pet">\n      <ion-segment-button value="puppies">\n        Terms\n      </ion-segment-button>\n      <ion-segment-button value="kittens">\n        Procedures\n      </ion-segment-button>\n      <ion-segment-button value="ducklings">\n        Well-being\n      </ion-segment-button>\n    </ion-segment>\n  </ion-toolbar>\n\n</ion-header>\n\n<ion-content>\n  <div [ngSwitch]="pet">\n    <ion-list *ngSwitchCase="\'puppies\'">\n      <ion-item>\n        <h2>Ruby</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Oscar</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Zoey</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Otto</h2>\n      </ion-item>\n    </ion-list>\n\n    <ion-list *ngSwitchCase="\'kittens\'">\n      <ion-item>\n        <h2>Luna</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Milo</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Bandit</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Nala</h2>\n      </ion-item>\n    </ion-list>\n\n    <ion-list *ngSwitchCase="\'ducklings\'">\n      <ion-item>\n        <h2>Daffy</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Huey</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Dewey</h2>\n      </ion-item>\n      <ion-item>\n        <h2>Louie</h2>\n      </ion-item>\n    </ion-list>\n  </div>\n</ion-content>'/*ion-inline-end:"/Users/mengyuxin/Desktop/Test/test/src/pages/contact/contact.html"*/
+        selector: 'page-education',template:/*ion-inline-start:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\education\education.html"*/'<ion-header >\n\n  <ion-navbar color="sblue">\n\n    <ion-title>\n\n      Education\n\n    </ion-title>\n\n  </ion-navbar>\n\n  <ion-toolbar color="sblue">\n\n     <ion-searchbar color="white"></ion-searchbar>\n\n  </ion-toolbar>\n\n\n\n\n\n\n\n<ion-toolbar no-border-top color="sblue">\n\n    <ion-segment [(ngModel)]="edu">\n\n      <ion-segment-button value="terms">\n\n        Terms\n\n      </ion-segment-button>\n\n      <ion-segment-button value="procedures">\n\n        Procedures\n\n      </ion-segment-button>\n\n      <ion-segment-button value="well-being">\n\n        Well-being\n\n      </ion-segment-button>\n\n    </ion-segment>\n\n  </ion-toolbar>\n\n\n\n</ion-header>\n\n\n\n<ion-content>\n\n  <div [ngSwitch]="edu">\n\n\n\n  <ion-item-group *ngSwitchCase="\'terms\'">\n\n      <ion-item-divider color="light">A</ion-item-divider>\n\n      <button ion-item detail-none (click)="buttonClick1()">\n\n          Aadffga\n\n      </button>\n\n      <p>{{content1}}</p>\n\n      <button ion-item detail-none (click)="buttonClick2()">\n\n          Afgsartsda\n\n      </button>\n\n      <p>{{content2}}</p>\n\n      <button ion-item detail-none (click)="buttonClick3()">\n\n          Aetef\n\n      </button>\n\n      <p>{{content3}}</p>\n\n  </ion-item-group>\n\n\n\n  <ion-item-group *ngSwitchCase="\'terms\'">\n\n      <ion-item-divider color="light">B</ion-item-divider>\n\n      <ion-item>Bangladesh</ion-item>\n\n      <ion-item>Belarus</ion-item>\n\n      <ion-item>Belgium</ion-item>\n\n      <ion-item>Bhutan</ion-item>\n\n      <ion-item>Bolivia</ion-item>\n\n      <ion-item>Brazil</ion-item>\n\n    </ion-item-group>\n\n\n\n  <ion-item-group *ngSwitchCase="\'procedures\'">\n\n      <ion-item-divider color="light">A</ion-item-divider>\n\n      <ion-item>Cambodia</ion-item>\n\n      <ion-item>Cameroon</ion-item>\n\n      <ion-item>Canada</ion-item>\n\n      <ion-item>Chile</ion-item>\n\n      <ion-item>China</ion-item>\n\n      <ion-item>Colombia</ion-item>\n\n      <ion-item>Costa Rica</ion-item>\n\n      <ion-item>Cuba</ion-item>\n\n      <ion-item>Cyprus</ion-item>\n\n      <ion-item>Czech Republic</ion-item>\n\n    </ion-item-group>\n\n\n\n    <ion-item-group *ngSwitchCase="\'well-being\'">\n\n      <ion-item-divider color="light">A</ion-item-divider>\n\n      <ion-item>Ajkdf</ion-item>\n\n      <ion-item>Adfbfd</ion-item>\n\n      <ion-item>Anjkjk</ion-item>\n\n      <ion-item>Aurueyi</ion-item>\n\n    </ion-item-group>\n\n\n\n  </div>\n\n</ion-content>'/*ion-inline-end:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\education\education.html"*/
     }),
-    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
-], ContactPage);
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]) === "function" && _a || Object])
+], EducationPage);
 
-//# sourceMappingURL=contact.js.map
+var _a;
+//# sourceMappingURL=education.js.map
 
 /***/ }),
 
@@ -179,7 +189,7 @@ var HomePage = (function () {
 }());
 HomePage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
-        selector: 'page-home',template:/*ion-inline-start:"/Users/mengyuxin/Desktop/Test/test/src/pages/home/home.html"*/'<ion-header >\n  <ion-navbar color="sblue">\n    <ion-title>Home</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding class="home">\n  <ion-grid>\n    <ion-row>\n    <ion-col>\n      <ion-fab>\n        <button ion-fab><ion-icon name="">25</ion-icon></button>\n      </ion-fab>\n    </ion-col>\n\n    <ion-col>\n      <ion-fab>\n        <button ion-fab><ion-icon name="">ss</ion-icon></button>\n      </ion-fab>\n    </ion-col>\n    </ion-row>\n\n<ion-fab bottom right>\n    <button ion-fab><ion-icon name="add"></ion-icon></button>\n    <ion-fab-list side="left">\n      <button ion-fab><ion-icon name="logo-facebook"></ion-icon></button>\n      <button ion-fab><ion-icon name="logo-twitter"></ion-icon></button>\n      <button ion-fab><ion-icon name="logo-vimeo"></ion-icon></button>\n      <button ion-fab><ion-icon name="logo-googleplus"></ion-icon></button>\n    </ion-fab-list>\n  </ion-fab>\n\n  </ion-grid>\n\n\n\n  \n</ion-content>\n'/*ion-inline-end:"/Users/mengyuxin/Desktop/Test/test/src/pages/home/home.html"*/
+        selector: 'page-home',template:/*ion-inline-start:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\home\home.html"*/'<ion-header >\n\n  <ion-navbar color="sblue">\n\n    <ion-title>Home</ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding class="home">\n\n  <ion-grid>\n\n    <ion-row>\n\n    <ion-col>\n\n      <ion-fab>\n\n        <button ion-fab><ion-icon name="">25</ion-icon></button>\n\n      </ion-fab>\n\n    </ion-col>\n\n\n\n    <ion-col>\n\n      <ion-fab>\n\n        <button ion-fab><ion-icon name="">ss</ion-icon></button>\n\n      </ion-fab>\n\n    </ion-col>\n\n    </ion-row>\n\n\n\n<ion-fab bottom right>\n\n    <button ion-fab><ion-icon name="add"></ion-icon></button>\n\n    <ion-fab-list side="left">\n\n      <button ion-fab><ion-icon name="logo-facebook"></ion-icon></button>\n\n      <button ion-fab><ion-icon name="logo-twitter"></ion-icon></button>\n\n      <button ion-fab><ion-icon name="logo-vimeo"></ion-icon></button>\n\n      <button ion-fab><ion-icon name="logo-googleplus"></ion-icon></button>\n\n    </ion-fab-list>\n\n  </ion-fab>\n\n\n\n  </ion-grid>\n\n\n\n\n\n\n\n  \n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\zjx_pc\Desktop\Test\test\src\pages\home\home.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
 ], HomePage);
@@ -212,7 +222,7 @@ Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* pl
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(33);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__(256);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_about_about__ = __webpack_require__(193);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_contact_contact__ = __webpack_require__(194);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_education_education__ = __webpack_require__(194);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home__ = __webpack_require__(195);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_tabs_tabs__ = __webpack_require__(192);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__ionic_native_status_bar__ = __webpack_require__(188);
@@ -243,7 +253,7 @@ AppModule = __decorate([
         declarations: [
             __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* MyApp */],
             __WEBPACK_IMPORTED_MODULE_4__pages_about_about__["a" /* AboutPage */],
-            __WEBPACK_IMPORTED_MODULE_5__pages_contact_contact__["a" /* ContactPage */],
+            __WEBPACK_IMPORTED_MODULE_5__pages_education_education__["a" /* EducationPage */],
             __WEBPACK_IMPORTED_MODULE_6__pages_home_home__["a" /* HomePage */],
             __WEBPACK_IMPORTED_MODULE_7__pages_tabs_tabs__["a" /* TabsPage */]
         ],
@@ -257,7 +267,7 @@ AppModule = __decorate([
         entryComponents: [
             __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* MyApp */],
             __WEBPACK_IMPORTED_MODULE_4__pages_about_about__["a" /* AboutPage */],
-            __WEBPACK_IMPORTED_MODULE_5__pages_contact_contact__["a" /* ContactPage */],
+            __WEBPACK_IMPORTED_MODULE_5__pages_education_education__["a" /* EducationPage */],
             __WEBPACK_IMPORTED_MODULE_6__pages_home_home__["a" /* HomePage */],
             __WEBPACK_IMPORTED_MODULE_7__pages_tabs_tabs__["a" /* TabsPage */]
         ],
@@ -310,7 +320,7 @@ var MyApp = (function () {
     return MyApp;
 }());
 MyApp = __decorate([
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({template:/*ion-inline-start:"/Users/mengyuxin/Desktop/Test/test/src/app/app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"/Users/mengyuxin/Desktop/Test/test/src/app/app.html"*/
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({template:/*ion-inline-start:"C:\Users\zjx_pc\Desktop\Test\test\src\app\app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n\n'/*ion-inline-end:"C:\Users\zjx_pc\Desktop\Test\test\src\app\app.html"*/
     }),
     __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* Platform */], __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */], __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */]])
 ], MyApp);
